8e8449115b93c4dbc94c707e97713e410e3cab55
sindrebenjamin <sindskog@gmail.com>
2024-05-04 14:37:10 +0200

    COMP: BookingCard

--- src/pages/AccountPage.tsx
+++ src/pages/AccountPage.tsx
@@ -1,15 +1,12 @@
-import HostCard from "../components/HostCard";
-import { useUserStore } from "../store/useUserStore";
+import BookingCard from "../components/BookingCard";
 
 const AccountPage = () => {
-  const user = useUserStore((state) => state.user);
   return (
     <>
-      <HostCard
-        name="Sindre"
-        mediaItem={user.avatar}
-        email="sindre@stud.noroff.no"
-        averageScore={4.4}
+      <BookingCard
+        title="Sindre Hansen"
+        bookings={3}
+        duration="Apr 23 - Apr 25, 2024"
       />
     </>
   );
